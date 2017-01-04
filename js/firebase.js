c161abc56be7039aca098c175a20d8c0ffdcd33c
Kirin Patel <kirinpatel@gmail.com>
2017-01-04 04:53:38 -0500

    Removed firebase file

--- js/firebase.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBF_VI6BQqDmYREJaan6BVwb_l3taPNpYc",
-    authDomain: "simple-chat-65457.firebaseapp.com",
-    databaseURL: "https://simple-chat-65457.firebaseio.com",
-    storageBucket: "simple-chat-65457.appspot.com",
-    messagingSenderId: "318625445752"
-};
-firebase.initializeApp(config);
